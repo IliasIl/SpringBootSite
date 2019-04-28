782cb06e42015832a2676cd54bab2085dd81b4e4
Ilias <cossin90@gmail.com>
2019-04-28 15:49:42 +0300

    Added new features.

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,4 +1,15 @@
 package com.example.sweater;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
 public class GreetingController {
+    @GetMapping("/")
+    public String getStart(Map<String, Object> model){
+        model.put("name", "Ilias");
+        return "main";
+    }
 }
--- src/main/java/com/example/sweater/repos/MessageRepo.java
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -1,4 +1,9 @@
 package com.example.sweater.repos;
 
-public class MessageRepo {
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepo extends CrudRepository<Message, Long> {
 }
