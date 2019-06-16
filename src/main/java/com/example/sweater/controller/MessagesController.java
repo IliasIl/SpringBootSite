78a41ecd31dfd932d93230b055a0925079995f41
Ilias <cossin90@gmail.com>
2019-06-17 00:01:59 +0300

    @PostMapping for edit messages is added

--- src/main/java/com/example/sweater/controller/MessagesController.java
+++ src/main/java/com/example/sweater/controller/MessagesController.java
@@ -1,11 +1,14 @@
 package com.example.sweater.controller;
 
+import com.example.sweater.domain.Message;
 import com.example.sweater.domain.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MessagesController {
@@ -15,6 +18,13 @@ public class MessagesController {
                              Model model) {
         model.addAttribute("isCurrent", currentUser.getId().equals(user.getId()));
         model.addAttribute("messages", user.getMessages());
+        return "userMessages";
+    }
+
+    @PostMapping("user-messages/{user}")
+    public String editMessage(@AuthenticationPrincipal User currentUser,
+                              @PathVariable User user,
+                              @RequestParam(required = false) Message message) {
         return "userMessages";
     }
 }
